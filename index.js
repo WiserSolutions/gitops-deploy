bfcc3bae96b090ae79102aea005077db429da446
Jordan Metzmeier <jmetzmeier01@gmail.com>
2020-12-17 12:12:53 -0600

    log what version gitops-deploy is setting

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ async function run(callback) {
     const repoPath = core.getInput('path');
     const ref = core.getInput('ref');
 
+    console.log(`Updating ${repoPath} to version ${versionToSet}`)
+
     const { contents, mode, commit, commitHash } = await getContents(repo, repoPath, ref);
 
     // update the contents with the new data
