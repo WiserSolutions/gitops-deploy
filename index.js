7a36d5d517750a7d86798d3289b0c79e4acdcb64
omer-tariq <mohammad.omer.tariq@outlook.com>
2023-07-08 00:08:17 -0400

    add retry

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const util = require('util');
+const util = require("util");
 
-const core = require('@actions/core');
+const core = require("@actions/core");
 
-const yaml = require('js-yaml');
+const yaml = require("js-yaml");
+const promiseRetry = require("promise-retry");
 
-const _get = require('lodash.get');
-const _set = require('lodash.set');
+const _get = require("lodash.get");
+const _set = require("lodash.set");
 
 async function readPath(repo, tree, path) {
-  const parts = path.split('/');
+  const parts = path.split("/");
 
   let cur = tree;
-  for(const p of parts.slice(0, parts.length - 1)) {
-    cur = await util.promisify(repo.loadAs)('tree', cur[p].hash);
+  for (const p of parts.slice(0, parts.length - 1)) {
+    cur = await util.promisify(repo.loadAs)("tree", cur[p].hash);
   }
 
   return {
     mode: cur[parts[parts.length - 1]].mode,
-    contents: await util.promisify(repo.loadAs)('blob', cur[parts[parts.length - 1]].hash)
+    contents: await util.promisify(repo.loadAs)(
+      "blob",
+      cur[parts[parts.length - 1]].hash
+    ),
   };
 }
 
 async function makeRepo(repoString, token) {
   const repo = {};
 
-  require('js-github/mixins/github-db')(repo, repoString, token);
-  require('js-git/mixins/create-tree')(repo);
+  require("js-github/mixins/github-db")(repo, repoString, token);
+  require("js-git/mixins/create-tree")(repo);
   //require('js-git/mixins/formats')(repo);
 
   return repo;
@@ -33,94 +37,115 @@ async function makeRepo(repoString, token) {
 
 async function getContents(repo, repoPath, ref) {
   var commitHash = await util.promisify(repo.readRef)(ref);
-  var commit = await util.promisify(repo.loadAs)('commit', commitHash);
-  var tree = await util.promisify(repo.loadAs)('tree', commit.tree);
+  var commit = await util.promisify(repo.loadAs)("commit", commitHash);
+  var tree = await util.promisify(repo.loadAs)("tree", commit.tree);
 
-  const {contents, mode} = await readPath(repo, tree, repoPath);
+  const { contents, mode } = await readPath(repo, tree, repoPath);
 
   const d = yaml.safeLoadAll(contents);
 
-  return { contents: d, mode, commit, commitHash }
+  return { contents: d, mode, commit, commitHash };
 }
 
 async function run(callback) {
   try {
+    const repo = await makeRepo(
+      core.getInput("repository"),
+      core.getInput("token")
+    );
 
-    const repo = await makeRepo(core.getInput('repository'), core.getInput('token'));
+    const versionToSet = core.getInput("new-version");
+    const repoPath = core.getInput("path");
+    const ref = core.getInput("ref");
 
-    const versionToSet = core.getInput('new-version');
-    const repoPath = core.getInput('path');
-    const ref = core.getInput('ref');
-    
-    if(!versionToSet) {
-        throw new Error('new-version is not set to anything, cannot update git');
+    if (!versionToSet) {
+      throw new Error("new-version is not set to anything, cannot update git");
     }
 
     console.log(`Updating ${repoPath} to version ${versionToSet}`);
 
     let contents, mode, commit, commitHash;
     try {
-        ({ contents, mode, commit, commitHash } = await getContents(repo, repoPath, ref));
-    } catch(err) {
-        console.log(util.inspect(err));
-        throw new Error(`could not read contents of configured gitops file, please make sure ${repoPath} exists!`);
+      ({ contents, mode, commit, commitHash } = await getContents(
+        repo,
+        repoPath,
+        ref
+      ));
+    } catch (err) {
+      console.log(util.inspect(err));
+      throw new Error(
+        `could not read contents of configured gitops file, please make sure ${repoPath} exists!`
+      );
     }
 
     // update the contents with the new data
     let newData;
-    const existingData = _get(contents, core.getInput('field'));
-    if(existingData.indexOf(':') !== -1) {
-        newData = existingData.substr(0, existingData.indexOf(':')) + ':' + versionToSet;
+    const existingData = _get(contents, core.getInput("field"));
+    if (existingData.indexOf(":") !== -1) {
+      newData =
+        existingData.substr(0, existingData.indexOf(":")) + ":" + versionToSet;
     } else {
-        newData = versionToSet;
+      newData = versionToSet;
     }
 
-    if(newData == existingData) {
-      console.log('nothing to commit, deploy is already set on gitops repo.');
+    if (newData == existingData) {
+      console.log("nothing to commit, deploy is already set on gitops repo.");
       return core.setOutput("commit", commitHash);
     }
 
-    const newContents = _set(contents, core.getInput('field'), newData);
+    const newContents = _set(contents, core.getInput("field"), newData);
 
     console.log(newContents);
 
-    const newFile = newContents.map((c) => yaml.safeDump(c, { noArrayIndent: true })).join('\n---\n');
+    const newFile = newContents
+      .map((c) => yaml.safeDump(c, { noArrayIndent: true }))
+      .join("\n---\n");
 
     const newTree = [
       {
         path: repoPath,
         mode: mode,
-        content: Buffer.from(newFile)
-      }
+        content: Buffer.from(newFile),
+      },
     ];
 
     newTree.base = commit.tree;
 
     const newTreeHash = await util.promisify(repo.createTree)(newTree);
 
-    console.log('created tree with hash:', newTreeHash);
+    console.log("created tree with hash:", newTreeHash);
 
-    var newCommitHash = await util.promisify(repo.saveAs)('commit', {
+    var newCommitHash = await util.promisify(repo.saveAs)("commit", {
       tree: newTreeHash,
       author: {
-        name: 'GitOps CI',
-        email: 'ci@example.com',
-        date: { seconds: Math.floor(Date.now() / 1000), offset: 0 }
+        name: "GitOps CI",
+        email: "ci@example.com",
+        date: { seconds: Math.floor(Date.now() / 1000), offset: 0 },
       },
       committer: {
-        name: 'GitOps CI',
-        email: 'ci@example.com',
-        date: { seconds: Math.floor(Date.now() / 1000), offset: 0 }
+        name: "GitOps CI",
+        email: "ci@example.com",
+        date: { seconds: Math.floor(Date.now() / 1000), offset: 0 },
       },
       parents: [commitHash],
-      message: `${process.env['GITHUB_REPOSITORY'].split('/')[1]}: new deploy (${versionToSet})`
+      message: `${
+        process.env["GITHUB_REPOSITORY"].split("/")[1]
+      }: new deploy (${versionToSet})`,
     });
 
-    console.log('created commit with hash:', newCommitHash);
+    console.log("created commit with hash:", newCommitHash);
+    promiseRetry(
+      async function (retry) {
+        try {
+          await util.promisify(repo.updateRef)(ref, newCommitHash);
+        } catch (error) {
+          retry();
+        }
+      },
+      { minTimeout: 1000, retries: 3 }
+    );
 
-    await util.promisify(repo.updateRef)(ref, newCommitHash);
-
-    console.log('updated ref');
+    console.log("updated ref");
 
     core.setOutput("commit", newCommitHash);
   } catch (error) {
